effdd7ebaf560c027f0c3ea18c0d8776de9889ae
Mingyi-Lin <mingyil3@illinois.edu>
2023-02-24 20:33:05 -0600

    Add section to app's menu

--- GPARecorder/GPARecorder/ContentView.swift
+++ GPARecorder/GPARecorder/ContentView.swift
@@ -22,6 +22,20 @@ struct ContentView: View {
     
 }
 
+struct RandomContent: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Random Text Here")
+        }.font(.title)
+            .padding();
+        
+        
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
