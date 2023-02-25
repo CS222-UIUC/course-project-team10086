7a67b0c938fcf41b680e26f91042f8d6793c4494
Kaifeng Lin <kaifeng6@illinois.edu>
2023-02-24 18:20:50 -0600

    first commit now

---
+++ GPARecorder/GPARecorder/GPARecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPARecorderApp.swift
+//  GPARecorder
+//
+//  Created by Mengqi Lin on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct GPARecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
